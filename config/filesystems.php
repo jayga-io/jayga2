89efb9904aaaf5f3c18ade3c3bd6d9213f4c151e
JohaaJnr <avoid.zoha@gmail.com>
2023-10-16 07:02:50 +0600

    new commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => '/home/jaygabdc/new.jayga.xyz',
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
             'throw' => false,
         ],
