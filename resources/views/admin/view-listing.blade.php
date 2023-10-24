70f1b27bec7526ba16e4bf2747d5efe7efe837cb
JohaaJnr <avoid.zoha@gmail.com>
2023-10-25 03:03:35 +0600

    test3

--- resources/views/admin/view-listing.blade.php
+++ resources/views/admin/view-listing.blade.php
@@ -40,8 +40,27 @@
                             <div class="card">
                                 @if (count($listing_images)>0)
                                     <!-- Carousel -->
-                                    <img src="https://new.jayga.xyz/uploads/listings/kZyUvFhvakT1rAToKjab84nFLjcSBN2uxIONS3Op.jpg" alt="Los Angeles"
-                                                class="d-block" style="width:100%">
+                                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                        
+                                        <div class="carousel-inner">
+                                            @foreach ($listing_images as $item)
+                                                <div class="carousel-item ">
+                                                    <img src="https://new.jayga.xyz/uploads/listings/kZyUvFhvakT1rAToKjab84nFLjcSBN2uxIONS3Op.jpg" alt="Image 1">
+                                                </div> 
+                                            @endforeach
+                                            
+                                           
+                                        </div>
+                                        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                    
                                 @else
                                     <p class="p-3 text-center">No listing image provided</p>
                                 @endif
