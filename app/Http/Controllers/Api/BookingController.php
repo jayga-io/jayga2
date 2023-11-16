0293ab3f3e214c294787e3b947a31e2ca2e9367a
JohaaJnr <avoid.zoha@gmail.com>
2023-11-16 06:57:05 +0600

    new

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Booking;
+use Illuminate\Support\Facades\Http;
 
 class BookingController extends Controller
 {
