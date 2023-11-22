6260f7b421e4f38c1e538d20b58e3a87683e957e
JohaaJnr <avoid.zoha@gmail.com>
2023-11-22 06:23:06 +0600

    new

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,12 @@ class AppServiceProvider extends ServiceProvider
     {
         $pending_count = Listing::where('isApproved', false)->get();
         $booking_count = Booking::where('isApproved', false)->get();
+        $booking = Booking::where('isApproved', true)->where('booking_status', 1)->get();
+        $pending = Booking::where('isApproved', false)->where('booking_status', 0)->get();
         view()->share('pending_count', $pending_count->count());
         view()->share('time', $pending_count);
         view()->share('booking_count', $booking_count->count());
+        view()->share('books', $booking->count());
+        view()->share('pends', $pending->count());
     }
 }
