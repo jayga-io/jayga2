16f60358877f8eb36e4cab125e4d439e076aee99
JohaaJnr <avoid.zoha@gmail.com>
2023-12-04 21:27:09 +0600

    new

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -17,20 +17,20 @@ class LoginController extends Controller
         $otp = random_int(1000,9999);
             
        
-            $url = 'https://sysadmin.muthobarta.com/api/v1/send-sms';
+          //  $url = 'https://sysadmin.muthobarta.com/api/v1/send-sms';
             
-            $user_api_url = env('APP_URL').'api/signin';
-            $phone = $request->input('phone');
-            $data = [
-                "sender_id" => "8809601010510",
-                "receiver" => $phone,
-                "message" => "Your Jayga OTP is:".$otp,
-                "remove_duplicate" => true
-            ];
-            $response = Http::withHeaders([
-                'Authorization' => 'Token d275d614a4ca92e21d2dea7a1e2bb81fbfac1eb0',
+          //  $user_api_url = env('APP_URL').'api/signin';
+          //  $phone = $request->input('phone');
+          //  $data = [
+          //      "sender_id" => "8809601010510",
+          //      "receiver" => $phone,
+          //      "message" => "Your Jayga OTP is:".$otp,
+          //      "remove_duplicate" => true
+          //  ];
+          //  $response = Http::withHeaders([
+          //      'Authorization' => 'Token d275d614a4ca92e21d2dea7a1e2bb81fbfac1eb0',
                 
-            ])->post($url, $data);
+          //  ])->post($url, $data);
 
            /* $user = Http::post($user_api_url, [
                'phone' => $phone,
