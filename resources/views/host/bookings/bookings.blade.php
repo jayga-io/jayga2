c831d8b23ff5a8d5364cd1232c3e0cc568d4b8ea
Atif_Karim <majorrip0@gmail.com>
2023-12-09 17:42:51 +0600

    tik

--- resources/views/host/bookings/bookings.blade.php
+++ resources/views/host/bookings/bookings.blade.php
@@ -223,14 +223,14 @@
 
     <script src="//cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script>
-        let table = new DataTable('#myTable'){
+        let table = new DataTable('#myTable',{
           scrollX: true
-        };
+        })
     </script>
     <script>
-        let table2 = new DataTable('#myTable2'){
+        let table2 = new DataTable('#myTable2',{
           scrollX: true
-        };
+        })
     </script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
