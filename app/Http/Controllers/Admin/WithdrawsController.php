9374f04ece233f140bba7953ff5509825d0a35a5
Atif_Karim <majorrip0@gmail.com>
2023-12-21 16:51:15 +0600

    changes

--- app/Http/Controllers/Admin/WithdrawsController.php
+++ app/Http/Controllers/Admin/WithdrawsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Withdraws;
+use App\Models\TransactionHistory;
 
 class WithdrawsController extends Controller
 {
@@ -14,7 +15,11 @@ class WithdrawsController extends Controller
     }
 
     public function mark_paid(Request $request, $id){
+        $amount = Withdraws::where('id', $id)->get();
         Withdraws::where('id', $id)->update([
+            'status' => true
+        ]);
+        TransactionHistory::where('transaction_amount', $amount[0]->withdraw_amount)->update([
             'status' => true
         ]);
         toastr()->addSuccess('Payment completed');
