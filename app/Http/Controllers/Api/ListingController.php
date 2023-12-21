35237133b257dcd9d58638853cc57d717575133c
Atif_Karim <majorrip0@gmail.com>
2023-12-21 13:48:05 +0600

    m

--- app/Http/Controllers/Api/ListingController.php
+++ app/Http/Controllers/Api/ListingController.php
@@ -410,7 +410,7 @@ class ListingController extends Controller
     }
 
     public function get_fav(Request $request, $id){
-        $favs = FavListing::where('user_id', $id)->with('listing')->get();
+        $favs = FavListing::where('user_id', $id)->with('listing.images')->get();
         return response()->json([
             'status' => 200,
             'Favourites' => $favs
