024e6aef42a9a711d5bfae2c532350520b89a4d4
Atif_Karim <majorrip0@gmail.com>
2023-12-21 13:56:08 +0600

    nn

--- app/Models/Reviews.php
+++ app/Models/Reviews.php
@@ -12,6 +12,6 @@ class Reviews extends Model
     protected $guarded = [];
 
     public function user(){
-        return $this->belongsTo(User::class, 'id', 'user_id');
+        return $this->hasOne(User::class, 'id', 'user_id');
     }
 }
