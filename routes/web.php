1494ff8d367d2f3ad946a856550bc558ab9d92a0
Atif_Karim <majorrip0@gmail.com>
2024-01-18 15:00:01 +0600

    new

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,32 @@ Route::get('/link/storage', function(){
     Artisan::call('storage:link');
 });
 
+
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache has been cleared';
+});
+
+Route::get('/route-cache', function() {
+    Artisan::call('route:cache');
+        return 'Routes cache has been cleared';
+    });
+
+
+//Clear config cache:
+
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    return 'Config cache has been cleared';
+  });
+
+  // Clear view cache:
+  
+  Route::get('/view-clear', function() {
+      Artisan::call('view:clear');
+      return 'View cache has been cleared';
+  });
+
 Route::prefix('admin')->group(function(){
 
     Route::get('/', function(){
