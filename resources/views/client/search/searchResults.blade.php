1718dce546bfc7cc79257bfa21171309d5ef7617
Atif_Karim <majorrip0@gmail.com>
2024-02-13 13:36:15 +0600

    new1

--- resources/views/client/search/searchResults.blade.php
+++ resources/views/client/search/searchResults.blade.php
@@ -7,7 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Epilogue">
     <style>
+
+        body {
+            font-family: "Epilogue";
+            overflow-x: hidden;
+        }
+
         .nav-link{
             color: #158E72;
         }
