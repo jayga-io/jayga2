22c0c1901d0af7b4e58addc0f891e474e27806d8
JohaaJnr <avoid.zoha@gmail.com>
2024-03-15 16:24:31 +0600

    new

--- app/Helpers/Sms.php
+++ app/Helpers/Sms.php
@@ -3,10 +3,17 @@
             
 function send_sms($data){
     $url = 'https://sysadmin.muthobarta.com/api/v1/send-sms';
+  // dd($data['sender_id']);
+    $details = [
+        "sender_id" => "8809617613331",
+        "receiver" => $data['receiver'],
+        "message" => $data['message'],
+        "remove_duplicate" => true
+    ];
     $response = Http::withHeaders([
         'Authorization' => 'Token d275d614a4ca92e21d2dea7a1e2bb81fbfac1eb0',
         
-    ])->post($url, $data);
+    ])->post($url, $details);
 
     return $response;
 }
--- resources/views/client/otp.blade.php
+++ resources/views/client/otp.blade.php
@@ -120,7 +120,7 @@
                         @csrf
                         <div class="form-group">
                             
-                            <input type="text" id="phone" name="OTP" class="form-control" placeholder="Enter the code we sent you {{$code}}" required>
+                            <input type="text" id="phone" name="OTP" class="form-control" placeholder="Enter the code we sent you" required>
                         </div>
                         <button type="submit" class="btn btn-warning" style="color: white;">Verify OTP</button>
                     </form>
