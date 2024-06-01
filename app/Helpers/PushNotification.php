39a4602f92f92116c9635421722a232ab7f66f9c
Atif_Karim <majorrip0@gmail.com>
2024-06-01 13:23:17 +0600

    check

--- app/Helpers/PushNotification.php
+++ app/Helpers/PushNotification.php
@@ -23,7 +23,7 @@ function send_notif($data){
         'Authorization' => 'Bearer ya29.a0AXooCgtKmcrEbhovMnPAZZ6v2kcEqoBC43NTQovyTt-huLWz5cZNLu4WWtC4MySDjNg-92-FwzBziqc30O7AJEH_nYhctLAlny989nlwvJLDVhJ36O-OEdpGWnogAicqf2zXDTzrtU7DsV7J5FApsh-aGjcGGNp2c5OsaCgYKAfQSARESFQHGX2Mi6LCWG5B-o8qxKsvoIVghCg0171',
     ])->post($url, $details);
 
-  //  dd($response);
+    dd($response);
 
     return $response;
 }
