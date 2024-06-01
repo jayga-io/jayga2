2f54653538c369dbbf06c1783afe8a08a6b4f9c5
Atif_Karim <majorrip0@gmail.com>
2024-06-01 13:21:55 +0600

    new test 2

--- app/Helpers/PushNotification.php
+++ app/Helpers/PushNotification.php
@@ -20,10 +20,10 @@ function send_notif($data){
     ];
 
     $response = Http::withHeaders([
-        'Authorization' => 'Bearer AAAAeOE4nqg:APA91bGSkxDP4cmqLb5supVEbHVmQBUIHH1NeKJsPkiGXTqPrGJmHxH6GRok8wgwUf7T8n_YBUmyzgqirs6Be1rI5nVO1hizAjBDQPImT7DvFG2Cln0xQW4xe6tZmqYWA8zJnwJIIhM6',
+        'Authorization' => 'Bearer ya29.a0AXooCgtKmcrEbhovMnPAZZ6v2kcEqoBC43NTQovyTt-huLWz5cZNLu4WWtC4MySDjNg-92-FwzBziqc30O7AJEH_nYhctLAlny989nlwvJLDVhJ36O-OEdpGWnogAicqf2zXDTzrtU7DsV7J5FApsh-aGjcGGNp2c5OsaCgYKAfQSARESFQHGX2Mi6LCWG5B-o8qxKsvoIVghCg0171',
     ])->post($url, $details);
 
-    dd($response);
+  //  dd($response);
 
     return $response;
 }
