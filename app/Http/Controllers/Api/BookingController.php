34c1bc5d96f3807033254714b7c540be450e1576
Atif_Karim <majorrip0@gmail.com>
2024-06-08 13:56:32 +0600

    new diss

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -115,7 +115,7 @@ class BookingController extends Controller
                 'guest_name' => $user[0]->name,
             ];
 
-           $notif_data = 'dsmLTBrWQomysqnaMUBxSe:APA91bE-iChS3YxkXbSms5eOXaB1nqlCmnAs983VpewGR-H_BLxNeaVk9rRD4xJWCKtylI9cFmqqWA4dBzWfZFp0k6yDzrr_B9tFgtIeEmh0aAu4j-nu5Iwmk6Fd2tqGl-d3PL58p88L';
+           $notif_data = $lister[0]->FCM_token;
            send_notif($notif_data);
 
            // Artisan::call('queue:listen');
