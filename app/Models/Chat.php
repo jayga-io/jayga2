c70aa6291c32d374cac0850ea60e35e9653622e1
JohaaJnr <avoid.zoha@gmail.com>
2024-08-21 13:54:34 +0600

    chat added

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -4,9 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Listing;
+use App\Models\Booking;
 
 class Chat extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function user(){
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    public function lister(){
+        return $this->hasOne(User::class, 'id', 'lister_id');
+    }
+
+    public function listing(){
+        return $this->hasOne(Listing::class, 'listing_id', 'listing_id');
+    }
+
+    public function booking(){
+        return $this->hasOne(Booking::class, 'booking_id', 'booking_id');
+    }
 }
