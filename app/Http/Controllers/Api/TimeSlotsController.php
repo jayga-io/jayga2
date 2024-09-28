d23a89c1e42d535d1decdcb020b422a0cd3a7040
Atif_Karim <majorrip0@gmail.com>
2024-09-28 14:14:47 +0600

    new push 3

--- app/Http/Controllers/Api/TimeSlotsController.php
+++ app/Http/Controllers/Api/TimeSlotsController.php
@@ -10,8 +10,9 @@ class TimeSlotsController extends Controller
 {
     public function timeslots(Request $request){
         $slots = TimeSlotShortstays::all();
-        Session(['pip'=>'J@yga2024']);
+        
         if(count($slots)>0){
+            Session(['pip'=>'J@yga2024']);
             return response()->json([
                 'status' => 200,
                 'timeslots' => $slots
